d72f1aba92d35807d3b4a2198231f30bf1a853bf
Roman Podymov <podymfrombryansk@yandex.ru>
2018-06-10 23:52:05 +0200

    Create ExtensionArray.swift

---
+++ ExtensionArray.swift
@@ -0,0 +1,22 @@
+extension Array {
+    
+    func allSatisfy(_ predicate: (Element) throws -> Bool) rethrows -> Bool {
+        
+        do {
+        
+            return try !self.contains {
+            
+                do {
+            
+                    return try !predicate($0)
+                } catch {
+
+                    throw error
+                }
+            }
+        } catch {
+            
+            throw error
+        }
+    }
+}
