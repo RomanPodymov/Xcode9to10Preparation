d84eaab50d6531e54a0e19c1b790030c52608992
RomanPodymov <podymfrombryansk@yandex.ru>
2018-06-27 09:04:24 +0200

    Carthage started

---
+++ Carthage/Carthage/Carthage.h
@@ -0,0 +1,19 @@
+//
+//  Carthage.h
+//  Carthage
+//
+//  Created by Роман Подымов on 27.06.2018.
+//  Copyright © 2018 roman.podymov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Carthage.
+FOUNDATION_EXPORT double CarthageVersionNumber;
+
+//! Project version string for Carthage.
+FOUNDATION_EXPORT const unsigned char CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Carthage/PublicHeader.h>
+
+
