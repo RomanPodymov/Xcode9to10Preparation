cf80554a81d1418be1757eba50f89a4ff88c5cb6
RomanPodymov <podymfrombryansk@yandex.ru>
2018-06-27 09:36:11 +0200

    new Carthage

---
+++ Xcode9to10Preparation/Xcode9to10Preparation/Xcode9to10Preparation.h
@@ -0,0 +1,19 @@
+//
+//  Xcode9to10Preparation.h
+//  Xcode9to10Preparation
+//
+//  Created by Roman Podymov on 27.06.2018.
+//  Copyright © 2018 roman.podymov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Xcode9to10Preparation.
+FOUNDATION_EXPORT double Xcode9to10PreparationVersionNumber;
+
+//! Project version string for Xcode9to10Preparation.
+FOUNDATION_EXPORT const unsigned char Xcode9to10PreparationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Xcode9to10Preparation/PublicHeader.h>
+
+
