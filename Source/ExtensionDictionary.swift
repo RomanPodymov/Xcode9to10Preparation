75358cc885c491072f72971b951980c230313572
RomanPodymov <podymfrombryansk@yandex.ru>
2018-10-14 12:03:50 +0200

    extension for Dictionary

---
+++ Source/ExtensionDictionary.swift
@@ -0,0 +1,15 @@
+//
+//  ExtensionDictionary.swift
+//  Pods
+//
+//  Created by Roman Podymov on 14.10.18.
+//
+
+#if XCODE_10_ENVIRONMENT
+#else
+public extension Dictionary {
+    func allSatisfy(_ predicate: ((key: Key, value: Value)) throws -> Bool) rethrows -> Bool {
+        return true
+    }
+}
+#endif
